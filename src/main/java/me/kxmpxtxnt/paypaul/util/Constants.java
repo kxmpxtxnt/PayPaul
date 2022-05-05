24fb9012409f802e8ab7e4c8536811397dd62a98
Paul Kindler <pa179kin@gmail.com>
2022-05-05 16:04:12 +0200

    remove some dumb ideas

--- src/main/java/me/kxmpxtxnt/paypaul/util/Constants.java
+++ src/main/java/me/kxmpxtxnt/paypaul/util/Constants.java
@@ -6,10 +6,4 @@ public class Constants {
     return "<gradient:#8656D7:#646AFF>================<reset><bold><white>" + name + "<reset><gradient:#646AFF:#8656D7>================<reset>";
   }
 
-  public static final String SEND_STATE = "send_state";
-  public static final String REQUEST_STATE = "send_state";
-
-  public static final String NEEDS_PLAYER = "needs_player";
-  public static final String NEEDS_MONEY = "needs_money";
-
 }
